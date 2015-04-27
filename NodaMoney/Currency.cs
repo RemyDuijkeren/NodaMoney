RemyDuijkeren/NodaMoney

commit 0fc3b479076e209e2f4383b0c3b262e51b9ad91d

Update decimal places for CLF currency to 4 to match cited sources here: http://www.currency-iso.org/en/home/tables/table-a1.html

NodaMoney/Currency.cs
@@ -317,7 +317,7 @@ namespace NodaMoney
                                      { "CHE", new Currency("CHE", "947", 2, "WIR Euro (complementary currency)", "CHE") },
                                      { "CHF", new Currency("CHF", "756", 2, "Swiss franc", "fr.") }, // or CHF
                                      { "CHW", new Currency("CHW", "948", 2, "WIR Franc (complementary currency)", "CHW") },
-                                     { "CLF", new Currency("CLF", "990", 0, "Unidad de Fomento (funds code)", "CLF") },
+                                     { "CLF", new Currency("CLF", "990", 4, "Unidad de Fomento (funds code)", "CLF") },
                                      { "CLP", new Currency("CLP", "152", 0, "Chilean peso", "$") },
                                      { "CNY", new Currency("CNY", "156", 2, "Chinese yuan", "¥") },
                                      { "COP", new Currency("COP", "170", 2, "Colombian peso", "$") },

commit dc7d346f37af120d202b91afe59e3cd7b2e96594

Made Currency class functional with 4 decimal places+
The previous limit was 3, so commit 0fc3b479076e209e2f4383b0c3b262e51b9ad91d broke the build
Not sure if 5 is a reasonable limit...

NodaMoney/Currency.cs
@@ -45,8 +45,8 @@ namespace NodaMoney
                 throw new ArgumentNullException("englishName");
             if (string.IsNullOrWhiteSpace(sign)) 
                 throw new ArgumentNullException("sign");
-            if (decimalDigits < -1 || decimalDigits > 3)
-                throw new ArgumentOutOfRangeException("code", "DecimalDigits must be between -1 and 3!");
+            if (decimalDigits < -1 || decimalDigits > 4)
+                throw new ArgumentOutOfRangeException("code", "DecimalDigits must be between -1 and 4!");
 
             Code = code;
             Number = number;

NodaMoney.UnitTests/CurrencyTests.cs
@@ -357,9 +357,9 @@ namespace NodaMoney.UnitTests
             }
 
             [TestMethod]
-            public void WhenDecimalDigitIsHigherThenThree_ThenCreatingShouldThrow()
+            public void WhenDecimalDigitIsHigherThenFour_ThenCreatingShouldThrow()
             {
-                Action action = () => { var eur = new Currency("EUR", "978", 4, "Euro", "€"); };
+                Action action = () => { var eur = new Currency("EUR", "978", 5, "Euro", "€"); };
 
                 action.ShouldThrow<ArgumentOutOfRangeException>();
             }