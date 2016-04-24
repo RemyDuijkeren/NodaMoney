3f586f585a92db5ae6ca8f4b4cb464e582ae518c
remyvd <remy.vanduijkeren@dynamichands.nl>
2016-04-24 18:28:15 +0200

    fix compile build 6

--- Build/default.ps1
+++ Build/default.ps1
@@ -64,11 +64,11 @@ Task RestoreNugetPackages {
 }
 
 Task Compile -depends RestoreNugetPackages {
-	$logger = if(isAppVeyor) { "/logger:""C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll""" }
+	$logger = if(isAppVeyor) { "/logger:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" }
 	
 	"$logger"
 
-	exec { msbuild "$RootDir\NodaMoney.sln" /t:Build /p:Configuration="Release" /p:Platform="Any CPU" /maxcpucount /verbosity:minimal /nologo "$logger" }
+	exec { msbuild "$RootDir\NodaMoney.sln" /t:Build /p:Configuration="Release" /p:Platform="Any CPU" /maxcpucount /verbosity:minimal /nologo $logger }
 }
 
 Task Test {
