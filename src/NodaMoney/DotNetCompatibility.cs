dc5d3538127037c530f1ea30481ec402b8e74aa1
Gunnar Liljas <gunnar.liljas@gmail.com>
2025-02-28 06:02:47 +0100

    NotNullWhenAttribute should be internal (#101)

--- src/NodaMoney/DotNetCompatibility.cs
+++ src/NodaMoney/DotNetCompatibility.cs
@@ -16,7 +16,7 @@ namespace System.Runtime.CompilerServices
 namespace System.Diagnostics.CodeAnalysis
 {
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    public sealed class NotNullWhenAttribute : Attribute
+    internal sealed class NotNullWhenAttribute : Attribute
     {
         public NotNullWhenAttribute(bool returnValue)
         {
