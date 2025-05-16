0d9bc88bcb1e7cc9df0fd1d3aaede5c3917635b8
RemyDuijkeren <remy@automatevalue.com>
2025-05-16 13:10:40 +0800

    Set default Precision value in MoneyContextOptions

--- src/NodaMoney/Context/MoneyContextOptions.cs
+++ src/NodaMoney/Context/MoneyContextOptions.cs
@@ -3,7 +3,7 @@ namespace NodaMoney.Context;
 public class MoneyContextOptions
 {
     public IRoundingStrategy RoundingStrategy { get; init; } = new StandardRounding();
-    public int Precision { get; init; }
-    public int? MaxScale { get; init; } = 28;
+    public int Precision { get; init; } = 28;
+    public int? MaxScale { get; init; }
     public CurrencyInfo? DefaultCurrency { get; init; }
 }
